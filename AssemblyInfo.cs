5533b3c3b59273c7ec511bd74f02754ad5d527b7
Zach Deibert <zachdeibert@gmail.com>
2016-04-18 16:06:58 -0500

    Initial commit.

---
+++ AssemblyInfo.cs
@@ -0,0 +1,24 @@
+﻿// AssemblyInfo.cs
+//
+// Copyright (c) 2016 Zach Deibert.
+// All Rights Reserved.
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyTitle("Com.Latipium.Defaults.SinglePlayer")]
+[assembly: AssemblyDescription("The default single player implementation for Latipium")]
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+[assembly: AssemblyCompany("Latipium")]
+[assembly: AssemblyProduct("Com.Latipium.Defaults.SinglePlayer")]
+[assembly: AssemblyCopyright("Zach Deibert")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("1.0.*")]
+
+[assembly: CLSCompliant(true)]
